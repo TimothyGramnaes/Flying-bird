04ce45d03bfb953249df2c9fbc213a1e8447ad13
Timothy Gramnaes <timothy.gramnaes@medieinstitutet.se>
2020-10-29 10:24:57 +0100

    Added comments

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 window.onload = start
+
+/**
+ * Let the position of the bird be raltive to the left side of the page
+ */
 let left = 0;
 
-
+/**
+ * Starts the program on page load.
+ */
 function start() { 
     fly();
 }
 
-
+/** Sets an interval to make the bird "fly" */
 function fly() {
     setInterval(moveBirdToTheRight, 10)
 
 }
 
+/** Moves the bird a small step to the right */
 function moveBirdToTheRight() {
     // Fetch bird from html
     const bird = document.querySelector('img');
